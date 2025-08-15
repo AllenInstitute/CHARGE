a4ca907a3a7313c753e37b464fbb0fc344b92cd0
Jeremy Miller <jeremym@alleninstitute.org>
2025-08-15 10:38:55 -0700

    Update available taxonomies

--- R/initialization.R
+++ R/initialization.R
@@ -2,27 +2,77 @@
 
 #... what is is called in the main page?
 table_names <- c(
-  "Macaque Basal Ganglia - preprint"
+  "Middle temporal gyrus (2022; NEW): SMART-seq",
+  "Middle temporal gyrus (2018): SMART-seq",
+  "Primary motor cortex (2020): SMART-seq",
+  "Multiple neocortical areas (2019): SMART-seq",
+  "Comparative LGN (2018): SMART-seq",
+  "Whole cortex and hippocampus (2021): 10X seq",
+  "Whole cortex and hippocampus (2021): SMART-seq",
+  "V1 & ALM (2018): SMART-seq",
+  "Comparative LGN (2018): SMART-seq",
+  "Comparative LGN (2018): SMART-seq"
 )
 
 #... what category each table is included in on the main page?
 categories <- factor(c(
-  "Cell type classification"
+  "Human brain cell types",
+  "Human brain cell types",
+  "Human brain cell types",
+  "Human brain cell types",
+  "Human brain cell types",
+  "Mouse brain cell types",
+  "Mouse brain cell types",
+  "Mouse brain cell types",
+  "Mouse brain cell types",
+  "Other mammalian brain cell types"
   ),
 # This is the order they will show up on in the list. **MAKE SURE THERE ARE NO TYPOS!**
-levels = c("Cell type classification")) 
+levels = c(
+  "Human brain cell types",
+  "Mouse brain cell types",
+  "Other mammalian brain cell types"
+  )
+) 
 
 
-#... where is the cell x annotation table?
-# Note that this can be a csv file, a feather directory, or a scrattch.taxonomy h5ad file.  It can also be local or on the web.
+#... where is the CHARGE.RData file?
+S3_folder = "s3://pga-genomics-wg-802451596237-us-west-2/CHARGE_data/test_CHARGE.RData"
 table_locations <- c(
-  "s3://pga-genomics-wg-802451596237-us-west-2/CHARGE_data/test_CHARGE.RData" #"../sandbox/test_CHARGE.RData"  # 
+  paste0(S3_folder,"Human_MTG_SEAAD_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Human_MTG_SMART_seq_08082025_CHARGE.RData"),
+  paste0(S3_folder,"Human_M1_10X_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Human_neocortex_SMART_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Human_LGN_SMART_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Mouse_cortex_hippocampus_10X_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Mouse_cortex_hippocampus_SMART_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Mouse_VISp_ALM_SMART_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Mouse_LGN_SMART_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Macaque_LGN_SMART_seq_04042025_CHARGE.RData")
 )
 
 
 descriptions   <- c(
 
-  "The Human and Mammalian Brain Atlas (HMBA) consortia has created a unified taxonomy of the mammalian basal ganglia, with >2 million cells collected from humans, macaques, and marmosets, and linkages to existing mouse brain data. More details are available here: https://alleninstitute.github.io/HMBA_BasalGanglia_Consensus_Taxonomy/.  The table here includes data about cell type assignments (cluster, group, subclass, class, and neighborhood) for the macaque taxonomy."
+  "Explore cell types in human middle temporal gyrus (MTG) circa 2025 as described in Gabitto, Travaglini, et al 2024 (Nature Neuroscience; https://doi.org/10.1038/s41593-024-01774-5)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/human-mtg-10x_sea-ad. Note that the data included here is from five adult healthy donors and does NOT include data from the 84 aged donors as part of the SEA-AD study (described at SEA-AD.org).",
+  
+  "Explore cell types in human middle temporal gyrus (MTG) circa 2018 as described in Hodge, Bakken, et al 2019 (Nature Neuroscience; https://doi.org/10.1038/s41586-019-1506-7)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/human-mtg-smart-seq.",
+  
+  "Explore cell types in human primary motor cortex (M1) circa 2021 as described in Bakken et al 2021 (Nature; https://doi.org/10.1038/s41586-021-03465-8)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/human-m1-10x, and are also included as part of the Cell Type Knowledge Explorer (https://knowledge.brain-map.org/celltypes).",
+  
+  "Explore DRAFT cell types in multiple human neocortical areas circa 2023. Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/human-multiple-cortical-areas-smart-seq. These cell types represent an OLD VERSION of the cell types described in Jorstad et al 2023 (Science; https://doi.org/10.1126/science.adf6812).",
+  
+  "Explore cell types in human lateral geniculate nucleus (LGN) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn.",
+  
+  "Explore cell types from multiple cortical areas and the hippocampal formation in mouse circa 2021 as described in Yao et al 2021 (Cell; https://doi.org/10.1016/j.cell.2021.04.021)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/mouse-whole-cortex-and-hippocampus-10x. These data include ~1.1 million cells collected using 10X Genomics droplet-based sequencing and are aligned to the same taxonomy as 'Whole Cortex and Hippocampus (2021): SMART seq'.",
+  
+  "Explore cell types from multiple cortical areas and the hippocampal formation in mouse circa 2021 as described in Yao et al 2021 (Cell; https://doi.org/10.1016/j.cell.2021.04.021)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/mouse-whole-cortex-and-hippocampus-smart-seq. These data include ~77 thousand cells collected using SMART sequencing and are aligned to the same taxonomy as 'Whole Cortex and Hippocampus (2021): 10x seq'.",
+  
+  "Explore cell types in mouse primary visual cortex (VISp) and anterior lateral motor cortex (ALM) circa 2018 as described in Tasic et al 2018 (Nature; https://doi.org/10.1038/s41586-018-0654-5)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/mouse-v1-and-alm-smart-seq.",
+  
+  "Explore cell types in mouse dorsolateral geniculate complex (LGd) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn.",
+  
+  "Explore cell types in macaque lateral geniculate nucleus (LGN) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn."
   
 )
 

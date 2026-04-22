fd68898246b6c34d94c64db7fb9e8612ec538d7a
Jeremy Miller <jeremym@alleninstitute.org>
2026-04-22 12:26:32 -0700

    Update initialization with new brain cell type data
    Added new table names, categories, and descriptions for various brain cell types across species, including human, macaque, and marmoset data.

--- R/initialization.R
+++ R/initialization.R
@@ -2,31 +2,35 @@
 
 #... what is is called in the main page?
 table_names <- c(
-  "Middle temporal gyrus (2022; NEW): SMART-seq",
-  "Middle temporal gyrus (2018): SMART-seq",
-  "Primary motor cortex (2020): SMART-seq",
-  "Multiple neocortical areas (2019): SMART-seq",
-  "Comparative LGN (2018): SMART-seq, human data",
-  "Whole cortex and hippocampus (2021): 10X seq",
-  "Whole cortex and hippocampus (2021): SMART-seq",
-  "V1 & ALM (2018): SMART-seq",
-  "Comparative LGN (2018): SMART-seq, mouse data",
-  "Consensus Basal Ganglia (2025): 10x seq, macaque data",
-  "Comparative LGN (2018): SMART-seq, macaque data"
+  "Consensus Basal Ganglia (2025): Human, 10x seq",
+  "Consensus Basal Ganglia (2025): Macaque, 10x seq",
+  "Consensus Basal Ganglia (2025): Marmoset, 10x seq",
+  "Human Middle temporal gyrus (2022): 10x seq",
+  "Human Middle temporal gyrus (2018): SMART-seq",
+  "Human Primary motor cortex (2020): SMART-seq",
+  "Human Multiple neocortical areas (2019): SMART-seq",
+  "Mouse Whole cortex and hippocampus (2021): 10X seq",
+  "Mouse Whole cortex and hippocampus (2021): SMART-seq",
+  "Mouse V1 & ALM (2018): , SMART-seq",
+  "Comparative LGN (2018): Human, SMART-seq",
+  "Comparative LGN (2018): Mouse, SMART-seq",
+  "Comparative LGN (2018): Macaque, SMART-seq"
 )
 
 #... what category each table is included in on the main page?
 categories <- factor(c(
   "Human brain cell types",
-  "Human brain cell types",
-  "Human brain cell types",
-  "Human brain cell types",
-  "Human brain cell types",
-  "Mouse brain cell types",
-  "Mouse brain cell types",
-  "Mouse brain cell types",
-  "Mouse brain cell types",
   "Other mammalian brain cell types",
+  "Other mammalian brain cell types",
+  "Human brain cell types",
+  "Human brain cell types",
+  "Human brain cell types",
+  "Human brain cell types",
+  "Mouse brain cell types",
+  "Mouse brain cell types",
+  "Mouse brain cell types",
+  "Human brain cell types",
+  "Mouse brain cell types",
   "Other mammalian brain cell types"
   ),
 # This is the order they will show up on in the list. **MAKE SURE THERE ARE NO TYPOS!**
@@ -41,21 +45,29 @@ levels = c(
 #... where is the CHARGE.RData file?
 S3_folder = "s3://pga-genomics-wg-802451596237-us-west-2/CHARGE_data/"
 table_locations <- c(
+  paste0(S3_folder,"Human_HMBA_basalganglia_AIT_pre-print_CHARGE.RData"),
+  paste0(S3_folder,"Macaque_HMBA_basalganglia_AIT_pre-print_CHARGE.RData"),
+  paste0(S3_folder,"Marmoset_HMBA_basalganglia_AIT_pre-print_CHARGE.RData"),
   paste0(S3_folder,"Human_MTG_SEAAD_04042025_CHARGE.RData"),
   paste0(S3_folder,"Human_MTG_SMART_seq_08082025_CHARGE.RData"),
   paste0(S3_folder,"Human_M1_10X_seq_04042025_CHARGE.RData"),
   paste0(S3_folder,"Human_neocortex_SMART_seq_04042025_CHARGE.RData"),
-  paste0(S3_folder,"Human_LGN_SMART_seq_04042025_CHARGE.RData"),
   paste0(S3_folder,"Mouse_cortex_hippocampus_10X_seq_04042025_CHARGE.RData"),
   paste0(S3_folder,"Mouse_cortex_hippocampus_SMART_seq_04042025_CHARGE.RData"),
   paste0(S3_folder,"Mouse_VISp_ALM_SMART_seq_04042025_CHARGE.RData"),
+  paste0(S3_folder,"Human_LGN_SMART_seq_04042025_CHARGE.RData"),
   paste0(S3_folder,"Mouse_LGN_SMART_seq_04042025_CHARGE.RData"),
-  paste0(S3_folder,"Macaque_BG_10X_seq_11112025_TEST_CHARGE.RData"),
   paste0(S3_folder,"Macaque_LGN_SMART_seq_04042025_CHARGE.RData")
 )
 
 
 descriptions   <- c(
+
+  "Explore cell types in HUMAN basal ganglia. These data are part of a cross-species consensus taxonomy released 11/13/2025 on Allen Brain Map. Download the data and explore the comprehensive, highly granular cell atlas spanning adult human, macaque, and marmoset brains that links brain structure, function and cellular architecture at https://brain-map.org/consortia/hmba/hmba-release-basal-ganglia. Note that the colors in these plots do not currently match reported cell type colors, and will be updated soon.",
+  
+  "Explore cell types in MACAQUE basal ganglia. These data are part of a cross-species consensus taxonomy released 11/13/2025 on Allen Brain Map. Download the data and explore the comprehensive, highly granular cell atlas spanning adult human, macaque, and marmoset brains that links brain structure, function and cellular architecture at https://brain-map.org/consortia/hmba/hmba-release-basal-ganglia. Note that the colors in these plots do not currently match reported cell type colors, and will be updated soon.",
+  
+  "Explore cell types in MARMOSET basal ganglia. These data are part of a cross-species consensus taxonomy released 11/13/2025 on Allen Brain Map. Download the data and explore the comprehensive, highly granular cell atlas spanning adult human, macaque, and marmoset brains that links brain structure, function and cellular architecture at https://brain-map.org/consortia/hmba/hmba-release-basal-ganglia. Note that the colors in these plots do not currently match reported cell type colors, and will be updated soon.",
 
   "Explore cell types in human middle temporal gyrus (MTG) circa 2025 as described in Gabitto, Travaglini, et al 2024 (Nature Neuroscience; https://doi.org/10.1038/s41593-024-01774-5)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/human-mtg-10x_sea-ad. Note that the data included here is from five adult healthy donors and does NOT include data from the 84 aged donors as part of the SEA-AD study (described at SEA-AD.org).",
   
@@ -65,17 +77,15 @@ descriptions   <- c(
   
   "Explore DRAFT cell types in multiple human neocortical areas circa 2023. Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/human-multiple-cortical-areas-smart-seq. These cell types represent an OLD VERSION of the cell types described in Jorstad et al 2023 (Science; https://doi.org/10.1126/science.adf6812).",
   
-  "Explore cell types in human lateral geniculate nucleus (LGN) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn.",
-  
   "Explore cell types from multiple cortical areas and the hippocampal formation in mouse circa 2021 as described in Yao et al 2021 (Cell; https://doi.org/10.1016/j.cell.2021.04.021)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/mouse-whole-cortex-and-hippocampus-10x. These data include ~1.1 million cells collected using 10X Genomics droplet-based sequencing and are aligned to the same taxonomy as 'Whole Cortex and Hippocampus (2021): SMART seq'.",
   
   "Explore cell types from multiple cortical areas and the hippocampal formation in mouse circa 2021 as described in Yao et al 2021 (Cell; https://doi.org/10.1016/j.cell.2021.04.021)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/mouse-whole-cortex-and-hippocampus-smart-seq. These data include ~77 thousand cells collected using SMART sequencing and are aligned to the same taxonomy as 'Whole Cortex and Hippocampus (2021): 10x seq'.",
   
   "Explore cell types in mouse primary visual cortex (VISp) and anterior lateral motor cortex (ALM) circa 2018 as described in Tasic et al 2018 (Nature; https://doi.org/10.1038/s41586-018-0654-5)! Underlying data and additonal visualizations are available at https://portal.brain-map.org/atlases-and-data/rnaseq/mouse-v1-and-alm-smart-seq.",
   
-  "Explore cell types in mouse dorsolateral geniculate complex (LGd) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn.",
+  "Explore cell types in human lateral geniculate nucleus (LGN) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn.",
   
-  "Explore cell types in macaque basal ganglia. These data are part of a cross-species consensus taxonomy just released (11/13/2025) on Allen Brain Map. Download the data and explore the comprehensive, highly granular cell atlas spanning adult human, macaque, and marmoset brains that links brain structure, function and cellular architecture at https://brain-map.org/consortia/hmba/hmba-release-basal-ganglia. Note that the statistics associated with this data set are still being adjusted.",
+  "Explore cell types in mouse dorsolateral geniculate complex (LGd) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn.",
   
   "Explore cell types in macaque lateral geniculate nucleus (LGN) circa 2021 as described in Bakken, van Velthoven, Menon, et al, 2021 (eLife; https://doi.org/10.7554/eLife.64875)! Underlying data are available at https://portal.brain-map.org/atlases-and-data/rnaseq/comparative-lgn."
   
